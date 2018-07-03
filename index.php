eed42a1eb7b8ea5703ecd2af2e38c65dfccd2d61
Dima <kotosyuk@gmail.com>
2018-07-03 21:50:50 +0300

    add files

--- index.php
+++ index.php
@@ -1,3 +1,104 @@
 
         <?php wp_head() ?>
+        <!-- Menu -->
+        <ul class=main-menu>
+                <li><a href=''>HOME</a></li>
+                <li><a href=''>About</a></li>
+                <li><a href=''>TTT 18</a></li>
+                <li><a href=''>Talents</a></li>
+                <li><a href=''>Coaches</a></li>
+                <li><a href=''>Partners</a></li>
+                <li><a href=''>Collaboration</a></li>
+                <li><a href=''>Contact</a></li>
+                <li><a href=''>Footer</a></li>
+        </ul>
+        <!-- Home -->
+        <!-- Banner image -->
+        <h1>UNLEASHING THE LIMITLESS POWER OF PUBLIC SPEAKING</h1> <!-- Primary text on banner -->
+        <p>Open movement since 2016</p> <!-- Secondary text on banner -->
+        <a href=''><span>Join the movement</span></a> <!-- navigate to the contact form (see contact). During the TTT application: leads to application form -->
+        <!-- Arrow element - navigating to scroll down. -->
+        <!-- ABOUT 
+        JOHANNES
+         Select images and put together the story of TTT and current way of things -->
+        <!-- TTT18 -->
+        <!-- Banner image -->
+        <h1>BUILDING THE NEXT GENERATION OF SPEAKER SUPERSTARS</h1> <!-- Primary text on banner -->
+        <p>Talk The Talk 2018 main program is all about finding the new generation of speaker talent, coaching them with the help of today’s master performers, weaving them into a community and giving them the opportunity to shine on stages all over the world.</p>
+        <p>Want to become a star? Here’s how.</p>
+        <!-- Arrow pointing down or some other indication to scroll down -->
+        <!-- Image for step 1 -->
+        <h1>STEP 1 – APPLY BY AUGUST 21.</h1> <!-- Primary text -->
+        <p>The application period opens on August 6th, and all you need is a topic you’re passionate about. Apply by filming a 1-minute video of you, speaking about your chosen topic. Tell us why it’s important to you and why the world should hear about it!</p>
+        <p>What we look on the video is (in order of importance):</p>
+        <ol>
+                <li> Your enthusiasm and motivation</li>
+                <li> The importance of your message - why should the world hear it</li>
+        </ol>
+        <p>Only then will we look at your performing ability</p>
+        <p>Note that we’re very strict on the video length. 1:00 is okay, 1:01 is too long. So make it short and sweet!</p>
+        <p>Before sending out the application, please mark the dates on your calendar and try to make sure you’ll be able to attend each of them.</p>
+        <p>We will inform all applicants on August 28.</p>
+        <!-- Image for step 2 -->
+        <h1>STEP 2 – LEARN FROM THE BEST.</h1> <!-- Primary text -->
+        <p>Application accepted, and you’re ready to rock! Here are the training dates for the fall. Joining each of them is recommendable, though not problematic if you miss one or two.</p>
+        <p>Note: Some of these dates &amp; hours might still change. We will confirm them by mid-August.</p>
+        <p>
+        3.9. 6-8 PM Kickoff<br>
+        4.9. 6-8 PM Star workshop: Storytelling<br>
+        11.9. 6-8 PM Training 1<br>
+        18.9. 6-8 PM Star workshop: Structure &amp; Rhetorics<br>
+        25.9. 6-8 PM Training 2<br>
+        2.10. 6-8 PM Star workshop: Emotion &amp; Contact<br>
+        4.10. 6-8 PM Training 3<br>
+        9.10. 6-8 PM Star workshop: Voice Techniques<br>
+        11.10. 6-8 PM Training 4<br>
+        16.10. 6-8 PM Star workshop: Finding Your Performing Style<br>
+        18.10. 6-8 PM Training 5<br>
+        23.10. 6-8 PM Star workshop: Improvisation<br>
+        25.10. 6-8 PM Training 6<br>
+        30.10. 6-8 PM Final Rehearsal 1<br>
+        6.11. 6-8 PM Final Rehearsal 2<br>
+        12.11. 6-9 PM Final Event
+        </p>
+        <p>Location for the trainings TBA. Also, make sure you are able to attend Slush 2018 on 4-5.12.</p>
+        <a href=''><span>Meet our coaches</span></a> <!-- leads to coach profiles -->
+        <!-- Image for step 3 -->
+        <h1>STEP 3 – SHINE AT THE FINALS.</h1> <!-- Primary text -->
+        <p>The final event will be held in Helsinki, at Musiikkitalo, on November the 12th. The event is open for everyone to join so expect your friends and family in the audience – and well, much more. Last year, the event welcomed hundreds of curious listeners and live stream lured thousands of viewers. This year, we are expecting over 1000 enthusiasts in the audience, from students to decision-makers, business leaders and influencers.</p>
+        <a href=''><span>See videos from last years</span></a> <!-- leads to last years’ videos (in case we don’t have them listed on our page, this can lead to our youtube channel) -->
+        <!-- Image for step 4 -->
+        <h1>STEP 4 – HOST A STAGE AT SLUSH.</h1> <!-- Primary text -->
+        <p>The best talents will get a chance to host a stage at Slush 2018 – in front of 20,000 people. You will form your own team together with other stage hosts and become a part of the Slush Team 2018.</p>
+        <p>Your role in making Slush a coherent, transformative experience cannot be underestimated – it will be a blast!</p>
+        <!-- Image for step 5 -->
+        <h1>STEP 5 – GATHER EXPERIENCE THROUGH COMMUNITY.</h1> <!-- Primary text -->
+        <p>These first four steps are only the prologue of your story – the real experience follows with all the speaking and performing possibilities enabled by the community. There’s always someone who needs an energetic host, a passionate storyteller or a motivational speaker for their event or project.</p>
+        <p>To maximize your future opportunities for getting stage time, we provide you with a platform through which these inquiries reach you – either directly or via us.</p>
+        <a href=''><span>Here you go</span></a> <!-- leads to talent profiles -->
+         <!-- Final event image -->
+         <h1>OPEN FOR EVERYONE.</h1> <!-- Primary text -->
+         <p>The final event will be held in Helsinki, at Musiikkitalo, on 12th November. Join over 1000 enthusiasts, see the best speakers of our generation and get inspired - or follow the spectacle via live stream.</p>
+         <p>See you there!</p>
+         <a href=''><span>Join the final event</span></a> <!-- leads to FB event (once published) -->
+         <a href=''><span>Watch the live stream</span></a> <!-- leads to livestream (once published) -->
+         <!-- TTT BACKSTAGE -->
+         <!-- Image 1 -->
+         <h1>Talk The Talk Backstage: we make people feel a little uncomfortable – but in a comfortable way!</h1> <!-- Big header --> 
+         <p>Talk The Talk Backstage is a concept for open workshops dedicated to the local community. In spring 2018 we designed and conducted three open workshops in Helsinki for a group of 30 participants each. With the feedback from participants we are ready to improve our materials even further for open workshops in the coming months.</p>
+         <!-- Image 2 -->
+         <p>TTT Backstage is always a free event with the hands-on attitude: WE speak less, YOU do more. The event is planned to be as an active practicing platform for public speaking. The venue is informal and atmosphere is relaxed and friendly.</p>
+         <!-- Image 3 -->
+         <p>Everyone can organise TTT Backstage following our guidelines. If you want to organise or help hosting TTT Backstage, contact us thorugh info@talkthetalk.fi</p>
+         <!-- TALENTS -->
+         <p>Talk The Talk main event provides talents with the platform for getting stage time. Anyone who needs a speaker or a performer can contact talents directly - if they prefer so. Additionally, TTT team also receives queries for gigs and projects which we then share to our talents. After all it’s practice and experience that make the difference.</p>
+         <!-- Two fields navigating to two different sub-pages. -->
+         <!-- PARTNERS -->
+         <!-- COLLABORATION -->
+         <!-- CONTACT -->
+         <!-- Image -->
+         <!-- Contact form --> <p>In case you want to join the movement, attend workshops or just say HI, drop us a message, we would love to hear from you! Just give us a moment to get back to you, and in the meanwhile take a look at our social media accounts and find like-minded people there.</p>
+         <!-- info@talkthetalk.fi -->
+         <!-- Social media buttons: Facebook, Instagram -->
+         <!-- Footer -->
         <?php wp_footer() ?>
